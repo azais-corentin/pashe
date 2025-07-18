2547e5e5c989c12321fe99974e3b891400e34121
Azaais Corentin <azaiscorentin@gmail.com>
2025-07-18 16:42:49 +0000

    Initial project setup with Docker and Rust

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    let guess = String::new();
+    let mut guess2 = String::new();
+
+    guess2.push('1');
+
+    println!("Guess: {}", guess);
+    println!("Guess2: {}", guess2);
+}
