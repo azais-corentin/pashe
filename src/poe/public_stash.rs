bbea5f902fa76668cd38d182e9c53c8c993f6836
Azaais Corentin <azaiscorentin@gmail.com>
2025-07-20 20:24:37 +0000

    Add rate limitation support

--- src/poe/public_stash.rs
+++ src/poe/public_stash.rs
@@ -1,42 +1,86 @@
 use crate::poe::constants::BASE_URL;
 use crate::poe::types::PublicStashTabs;
 use anyhow::{Context, Result};
-use oauth2::http::response;
-use std::fs;
 use tracing::{debug, error};
 
-pub async fn fetch_public_stashes(
-    client: &reqwest::Client,
-    next_change_id: &str,
-) -> Result<PublicStashTabs> {
-    let url = format!("{}/public-stash-tabs?id={}", BASE_URL, next_change_id);
+pub struct Crawler {
+    stash_count: u64,
+    item_count: u64,
+    bytes: u64,
+}
 
-    debug!("Fetching public stashes: {}", url);
-
-    let response = client.get(url.clone()).send().await?;
-
-    if response.status() != reqwest::StatusCode::OK {
-        let status = response.status();
-        error!("Failed to fetch public stashes: HTTP {}", status);
-        return Err(anyhow::anyhow!(
-            "Failed to fetch public stashes: HTTP {}",
-            status
-        ));
-    }
-
-    let text_body = response
-        .text()
-        .await
-        .with_context(|| format!("Failed to read response body from {}", url))?;
-
-    let _span = tracing::debug_span!("Parsing response body");
-    let stashes = match serde_json::from_str::<PublicStashTabs>(&text_body) {
-        Ok(stashes) => stashes,
-        Err(e) => {
-            error!("Failed to parse response: {}", e);
-            return Err(anyhow::anyhow!("Failed to parse response: {}", e));
+impl Default for Crawler {
+    fn default() -> Self {
+        Crawler {
+            stash_count: 0,
+            item_count: 0,
+            bytes: 0,
         }
-    };
+    }
+}
 
-    Ok(stashes)
+pub trait Fetch {
+    async fn fetch_public_stashes(
+        &mut self,
+        client: &reqwest_middleware::ClientWithMiddleware,
+        next_change_id: &str,
+    ) -> Result<PublicStashTabs>;
+}
+
+impl Fetch for Crawler {
+    async fn fetch_public_stashes(
+        &mut self,
+        client: &reqwest_middleware::ClientWithMiddleware,
+        next_change_id: &str,
+    ) -> Result<PublicStashTabs> {
+        let url = format!("{}/public-stash-tabs?id={}", BASE_URL, next_change_id);
+
+        debug!("Fetching public stashes: {}", url);
+
+        let response = client.get(url.clone()).send().await?;
+
+        if response.status() != reqwest::StatusCode::OK {
+            let status = response.status();
+            error!("Failed to fetch public stashes: HTTP {}", status);
+            return Err(anyhow::anyhow!(
+                "Failed to fetch public stashes: HTTP {}",
+                status
+            ));
+        }
+
+        // TODO: Read x-next-change-id header and schedule the next request ASAP
+
+        let text_body = response
+            .text()
+            .await
+            .with_context(|| format!("Failed to read response body from {}", url))?;
+
+        debug!("Got response body");
+
+        let _span = tracing::debug_span!("Parsing response body");
+        let stashes = match serde_json::from_str::<PublicStashTabs>(&text_body) {
+            Ok(stashes) => stashes,
+            Err(e) => {
+                error!("Failed to parse response: {}", e);
+                return Err(anyhow::anyhow!("Failed to parse response: {}", e));
+            }
+        };
+
+        self.stash_count += stashes.stashes.len() as u64;
+        self.item_count += stashes
+            .stashes
+            .iter()
+            .map(|s| s.items.len() as u64)
+            .sum::<u64>();
+        self.bytes += text_body.len() as u64;
+
+        debug!(
+            "Total stats: {} stashes / {} items / {}",
+            self.stash_count,
+            self.item_count,
+            bytesize::ByteSize::b(self.bytes).display().si()
+        );
+
+        Ok(stashes)
+    }
 }
---
+++ src/poe/rate_limiting.rs
@@ -0,0 +1,153 @@
+use anyhow::{Context, Result};
+use http::{Extensions, HeaderMap};
+use reqwest::{Request, Response};
+use reqwest_middleware::{Middleware, Next};
+use tracing::{debug, info, warn};
+
+#[derive(Debug)]
+pub struct RateLimits {
+    pub remaining_requests: u32,
+    pub reset_date: tokio::time::Instant,
+    pub period: u32,
+}
+
+/// A simple rate limiter that is sufficient for our use case.
+/// It only works well if there's only one request in transit at a time.
+trait RateLimiter {
+    async fn ensure(&mut self);
+    fn update(&mut self, response: &Response) -> Result<()>;
+}
+
+fn split_header_to_vec<'a>(headers: &'a HeaderMap, header_name: &str) -> Result<Vec<&'a str>> {
+    Ok(headers
+        .get(header_name)
+        .ok_or_else(|| anyhow::anyhow!("{} header not found", header_name))?
+        .to_str()?
+        .split(':')
+        .collect())
+}
+
+impl RateLimiter for RateLimits {
+    async fn ensure(&mut self) {
+        let limited = self.remaining_requests == 0 || self.reset_date > tokio::time::Instant::now();
+
+        if limited {
+            // If there's no reset date, update it from the period and set it to handle the worst case
+            if self.reset_date <= tokio::time::Instant::now() {
+                self.reset_date = tokio::time::Instant::now()
+                    + tokio::time::Duration::from_secs(self.period as u64);
+            }
+
+            warn!(
+                "Rate limit reached, sleeping until {}",
+                chrono::Utc::now()
+                    + chrono::Duration::from_std(
+                        self.reset_date
+                            .saturating_duration_since(tokio::time::Instant::now()),
+                    )
+                    .unwrap()
+            );
+            tokio::time::sleep_until(self.reset_date).await;
+            debug!("Resuming after rate limit sleep");
+        }
+
+        self.remaining_requests = self.remaining_requests.saturating_sub(1);
+    }
+
+    fn update(&mut self, response: &Response) -> Result<()> {
+        let headers = response.headers();
+
+        if !headers.contains_key("X-Rate-Limit-Ip")
+            || !headers.contains_key("X-Rate-Limit-Ip-State")
+        {
+            warn!("Response headers do not contain rate limit information, skipping update");
+            return Ok(());
+        }
+
+        let ip_rate_limits = split_header_to_vec(headers, "X-Rate-Limit-Ip")?;
+        let ip_rate_limits_state = split_header_to_vec(headers, "X-Rate-Limit-Ip-State")?;
+
+        debug!("Headers: {:?}", headers);
+
+        if ip_rate_limits.len() != 3 {
+            return Err(anyhow::anyhow!(
+                "X-Rate-Limit-Ip header has invalid format '{}'",
+                ip_rate_limits.join(":")
+            ));
+        }
+        if ip_rate_limits_state.len() != 3 {
+            return Err(anyhow::anyhow!(
+                "X-Rate-Limit-Ip-State header has invalid format '{}'",
+                ip_rate_limits_state.join(":")
+            ));
+        }
+
+        let max_hits = ip_rate_limits[0]
+            .parse::<u32>()
+            .context("Failed to parse max hits from X-Rate-Limit-Ip header")?;
+        let period = ip_rate_limits[1]
+            .parse::<u32>()
+            .context("Failed to parse period from X-Rate-Limit-Ip header")?;
+        // let time_restriction = ip_rate_limits[2]
+        //     .parse::<u32>()
+        //     .context("Failed to parse time restriction from X-Rate-Limit-Ip header")?;
+
+        let current_hit_count = ip_rate_limits_state[0]
+            .parse::<u32>()
+            .context("Failed to parse current hit count from X-Rate-Limit-Ip-State header")?;
+        // let tested_period = ip_rate_limits_state[1]
+        //     .parse::<u32>()
+        //     .context("Failed to parse tested period from X-Rate-Limit-Ip-State header")?;
+        self.reset_date =
+            tokio::time::Instant::now() + tokio::time::Duration::from_secs(period as u64);
+
+        self.remaining_requests = max_hits - current_hit_count;
+        self.period = period;
+
+        Ok(())
+    }
+}
+
+pub struct RateLimitingMiddleware {
+    pub limits: tokio::sync::Mutex<RateLimits>,
+}
+
+impl Default for RateLimits {
+    fn default() -> Self {
+        Self {
+            remaining_requests: 1,
+            reset_date: tokio::time::Instant::now(),
+            period: 0,
+        }
+    }
+}
+
+impl Default for RateLimitingMiddleware {
+    fn default() -> Self {
+        Self {
+            limits: tokio::sync::Mutex::new(RateLimits::default()),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl Middleware for RateLimitingMiddleware {
+    async fn handle(
+        &self,
+        req: Request,
+        extensions: &mut Extensions,
+        next: Next<'_>,
+    ) -> reqwest_middleware::Result<Response> {
+        info!("Running request...");
+        self.limits.lock().await.ensure().await;
+        let response = next.run(req, extensions).await;
+
+        if let Ok(response) = response.as_ref() {
+            self.limits.lock().await.update(response)?;
+        }
+
+        info!("Done with request");
+
+        response
+    }
+}
