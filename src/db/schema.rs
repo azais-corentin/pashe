26ea5ad7378006f678641e959169c5ec34adc7ed
Azaais Corentin <azaiscorentin@gmail.com>
2025-07-21 07:25:06 +0000

    refactor(db): update future database schema
    - Modify schema.rs to prepare for upcoming database changes
    - Ensure compatibility with future migration requirements

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,34 +1,62 @@
 /*
 Table accounts {
-  id integer [increment]
+  id UInt64 [increment]
   name string [note: 'accountName']
 }
 
 Table stashes {
   id FixedString(64)
   name String
-  account_id integer [ref: > accounts.id]
+  account_id UInt64 [ref: > accounts.id]
 }
 
 Table items {
   id FixedString(64)
-  timestamp datetime [default: `now()`]
+  timestamp DateTime [default: `now()`]
   realm LowCardinality(String)
   stash_id FixedString(64) [ref: > stashes.id]
   name LowCardinality(String)
   base LowCardinality(String)
-  links integer
-  ilvl integer
+  links UInt8
+  ilvl UInt8
   corrupted bool
-  stack_size integer
+  stack_size UInt32
   // For gems
-  level integer
-  quality integer
+  level UInt8
+  quality Int16
   // cluster jewels
-  passives integer
+  passives UInt8
   // maps and essences
-  tier integer
+  tier UInt8
   // base types
   influences "Array(LowCardinality(String))"
 }
+
+Table statistics_events {
+  timestamp DateTime [default: `now()`]
+  stash_count UInt64
+  item_count UInt64
+  bytes UInt64
+}
+
+Enum period_types {
+  total
+  year
+  day
+  hour
+}
+
+Table statistics_per_periods {
+  period_type period_types
+  period_start DateTime
+  total_stash_count UInt64
+  total_item_count UInt64
+  total_bytes UInt64
+}
+*/
+
+/*
+Use a MergeTree + SummingMergeTree with a materialized view to maintain real-time sums in ClickHouse.
+
+https://g.co/gemini/share/5f338e009cf4
 */
