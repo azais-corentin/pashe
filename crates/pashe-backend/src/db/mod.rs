bf169d5db4536a315cc4f40d096f164e826628a0
Corentin AZAIS <c.azais@ikalogic.com>
2025-07-28 12:05:27 +0000

    fix(backend): resolve clippy lints in database module

--- crates/pashe-backend/src/db/mod.rs
+++ crates/pashe-backend/src/db/mod.rs
@@ -3,5 +3,4 @@ mod error;
 mod schema;
 
 pub use client::Client;
-pub use error::Error;
-pub use schema::{Item, PeriodType, SchemaMigration, StatisticsEvent, StatisticsPerPeriod};
+pub use schema::{Item, StatisticsEvent};
